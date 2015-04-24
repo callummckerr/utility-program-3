25ac14d1645de8e5ff1e9ef2295d7d7621f5182f
callummckerr <callum@mckerrs.net>
2015-04-24 14:09:48 +1000

    Update utilityprogram.bat

--- utilityprogram.bat
+++ utilityprogram.bat
@@ -1,6 +1,13 @@
 @echo off
 REM prints time and date
-echo %TIME%
+echo %TIME%,
 echo %DATE%
 echo
-pause
+REM waits for 5 seconds
+timeout /5
+cls
+REM Draws menu
+echo UTILITY PROGRAM
+echo ping
+REM begins menu system
+SET /p 
